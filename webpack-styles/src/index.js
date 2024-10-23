c347e3f5452a4dcad803ff6f0b5a4cacba8148c3
Silas larsen <silaslarsen80@gmail.com>
2024-10-23 09:43:55 +0200

    add more

--- webpack-styles/src/index.js
+++ webpack-styles/src/index.js
@@ -1,4 +1,4 @@
-import './style.css'
-
+import './style.scss';
+import './woof.css';
 const headline = 'Welcome to the webpage';
 document.querySelector('h1').innerText = headline;
--- webpack-styles/webpack.config.js
+++ webpack-styles/webpack.config.js
@@ -1,13 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports =  {
+    mode: "development",
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
     module:{
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.(s[ac]|c)ss$/i,
                 use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            }
-        ]
-    }
-}
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader",
+                ],
+            },
+        ],
+    },
+    devtool: 'source-map',
+};
